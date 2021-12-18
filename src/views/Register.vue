cb9f9baf5826569beae3d0ccc9442676d8a4a9e6
Ruslan <rustimka1@gmail.com>
2021-12-18 19:38:20 +0200

    Fich  Register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,54 @@
 <template>
-    <div>Register page</div>
+    <div class="auth-page" >
+        <div class="container page">
+            <div class="row">
+                <div class="col-md-6 ofset-md-3 col-xs-12">
+                    <h1 class="text-xs-center">Sign Up</h1>
+                    <p class="text-xs-center">
+                        <router-link :to="{name: 'login'}">Need an account?</router-link>
+                    </p>   
+                    VALIDATION ERRORS
+                    <form @submit.prevent= "onSubmit">
+                        <fieldset class="form-group">
+                            <input 
+                            type="text" 
+                            class="form-control-lg" 
+                            placeholder="Username"
+                            />
+                        </fieldset>
+
+                        <fieldset class="form-group">
+                            <input  
+                            type="text"
+                            class="form-control-lg"
+                            placeholder="Email"
+                            />
+                        </fieldset>
+
+                        <fieldset class="form-group">
+                            <input 
+                            type="password"
+                            class="form-control-lg"
+                            placeholder="Password"
+                            />
+                        </fieldset>
+                        <button class="bt bt-lg btn-primary pull-xs-rigt">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-name: 'McvRegister'
-
+    name: 'McvRegister',
+    methods : {
+        onSubmit() {
+            console.log('subbmitted form')
+        }
+    }
 }
 </script>
