4ea0c9c34cfaaeddc1ac7e32ad6710805bbfceb8
Ruslan <rustimka1@gmail.com>
2021-12-26 15:33:30 +0200

    Validations Errors

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,35 +3,36 @@ import {setItem} from '@/helpers/persistanceStorage'
 
 const state = {
   isSubmitting: false,
-  carrentUser: null,
-  validdationErrors: null,
-  isLoggedIn: null 
+  isLoggedIn: null,
+  currentUser: null,
+  validationErrors: null
 }
 
 const mutations = {
   registerStart(state) {
-    state.isSubmitting = true,
-    state.validdationErrors = null
+    state.isSubmitting = true
+    state.validationErrors = null
   },
   registerSuccess(state, payload) {
-    state.isSubmitting = false,
-    state.carrentUser = payload,
+    state.isSubmitting = false
     state.isLoggedIn = true
+    state.currentUser = payload
   },
   registerFailure(state, payload) {
-    state.isSubmitting = false,
-    state.validdationErrors = payload
+    state.isSubmitting = false
+    state.validationErrors = payload
   }
 }
 
 const actions = {
   register(context, credentials) {
     return new Promise(resolve => {
+      context.commit('registerStart')
       authApi
         .register(credentials)
         .then(response => {
           context.commit('registerSuccess', response.data.user)
-          setItem('accessToken',response.data.user.token)
+          setItem('accessToken', response.data.user.token)
           resolve(response.data.user)
         })
         .catch(result => {
@@ -45,5 +46,5 @@ export default {
   state,
   actions,
   mutations
-  
+  // getters
 }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,81 +1,95 @@
 <template>
-    <div class="auth-page" >
-        <div class="container page">
-            <div class="row">
-                <div class="col-md-6 offset-md-3 col-xs-12">
-                    <h1 class="text-xs-center">Sign Up</h1>
-                    <p class="text-xs-center">
-                        <router-link :to="{name: 'login'}">Need an account?</router-link>
-                    </p>   
-                    VALIDATION ERRORS
-                    <form @submit.prevent= "onSubmit">
-                        <fieldset class="form-group">
-                            <input 
-                                type="text" 
-                                class="form-control form-control-lg" 
-                                placeholder="Username"
-                                v-model="username"
-                            />
-                        </fieldset>
-
-                        <fieldset class="form-group">
-                            <input  
-                                type="text"
-                                class="form-control form-control-lg"
-                                placeholder="Email"
-                                v-model="email"
-                            />
-                        </fieldset>
-
-                        <fieldset class="form-group">
-                            <input 
-                                type="password"
-                                class="form-control form-control-lg"
-                                placeholder="Password"
-                                v-model="password"
-                            />
-                        </fieldset>
-                        <button class="btn btn-lg btn-primary pull-xs-right" 
-                        :disabled="isSubmitting"
-                        >
-                            Sign Up
-                        </button>
-                    </form>        
-                </div>
-            </div>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'login'}">
+              Need an account?
+            </router-link>
+          </p>
+          <mcv-validation-errors
+           v-if="validationErrors"
+          :validation-errors="validationErrors" 
+          />
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Username"
+                v-model="username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+                v-model="email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                v-model="password"
+              />
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign Up
+            </button>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import McvValidationErrors from '@/components/ValidationErrors'
 export default {
-    name: 'McvRegister',
-    data() {
-        return {
-            email: '',
-            password: '',
-            username: ''
-        }
-    },
-    computed: {
-        isSubmitting(){
-            return this.$store.state.auth.isSubmitting
-        }
-    },
-    methods : {
-        onSubmit() {
-            console.log('subbmitted form')
-            this.$store
-                .dispatch('register', {
-                    email: this.email, 
-                    username: this.username, 
-                    paswword: this.password
-                })
-                .then(user => {
-                console.log('successfully register user', user)
-                this.$router.push({name: 'home'})
-                })
-        }
+  name: 'McvRegister',
+  components: {
+    McvValidationErrors
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: ''
     }
+  },
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    },
+    validattionErrors() {
+      return this.$store.state.auth.validattionErrors
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('onSubmit')
+      this.$store
+        .dispatch('register', {
+          email: this.email,
+          username: this.username,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({name: 'home'})
+        })
+    }
+    /* increaseCounter() { */
+    /*   console.log('increaseCounter') */
+    /*   this.$store.commit('increment') */
+    /* } */
+  }
 }
 </script>
